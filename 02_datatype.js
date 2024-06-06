cf05c711b582ef2832c0087f9fcda996a15ee3e0
DipKhatik <khatikdip3112@gmail.com>
2024-06-07 01:48:40 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,23 @@
+// ------------------------------Primitive data types------------------------------------
+
+// number => 0, 1, 2, 3, 4
+// string => ""
+// bigint => large number/value
+// boolean => true, false
+// null => stand alone value
+// undefined => undefined (kuch nahi)
+// symbol => unique value(used on figma, react)
+
+
+// ------------------------------Non-Primitive data types------------------------------------
+// array => []
+// object => {}
+// function => functions
+
+let name = "hitesh";
+let age = 22;
+let isMarried = false;
+
+
+//console.log(typeof null); null data type is an object
+console.log(typeof undefined); //undefined
