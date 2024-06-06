2b2a2c55165d052db7369c26b91285839b0c4b7a
DipKhatik <khatikdip3112@gmail.com>
2024-06-07 02:50:59 +0530

    Add files via upload

---
+++ 04_operations.js
@@ -0,0 +1,41 @@
+//------------------------------operations---------------------------------
+let value = 10;
+let negValue = -value;  
+
+// console.log(negValue);
+
+// console.log(2+2);   addition
+// console.log(2-2);   subtraction
+// console.log(2*2);    multiplication
+// console.log(2**3);   power
+// console.log(2/2);    division
+// console.log(2%2);    per
+
+
+
+//------------------------------adding string---------------------------------
+let firstName = "hitesh";
+let lastName = " kumar";
+// console.log(firstName + lastName);
+
+
+
+// //-------------------------------interview questions--------------------------------
+// console.log("2" + 2);            // prom string
+// console.log(2 + "2");            //prom string
+// console.log("1" + 2 + 2);        //prom string
+// console.log(2 + 1 + "1");        //add+ string
+//--------------------------------not way to write such type of--------------------------------- 
+
+
+// console.log(+true);//1
+// console.log(+""); //0
+
+// let gameCounter = 100;
+// gameCounter++;               101
+// console.log(gameCounter);
+
+
+let gameCounter = 100;
+++gameCounter;  
+console.log(gameCounter);
