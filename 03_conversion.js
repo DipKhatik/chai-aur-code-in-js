2907c2db4f5498a3e6b8980b248a38c811db73e0
DipKhatik <khatikdip3112@gmail.com>
2024-06-07 02:09:49 +0530

    Add files via upload

---
+++ 03_conversion.js
@@ -0,0 +1,31 @@
+//----------------------------number to string----------------------------------------
+
+let score = 50;
+// console.log(typeof score);
+let dip = String(score);
+// console.log(typeof dip);
+// console.log(dip);
+
+//----------------------------conversion----------------------------------------
+//"33"=> 33
+//"33abc" => NaN
+// true => 1
+// false => 0
+//null => 0
+//undefined => NaN
+//"" =>  false
+//"dip" => true
+
+
+//------------------------------boolean------------------------------------------
+let isLoggedIn = 1;
+let boolean = Boolean(isLoggedIn);
+//console.log(boolean);
+
+//-------------------------------string---------------------------------------------
+
+let someNumber = 33;
+let stringNo = String(someNumber);
+console.log(stringNo);
+console.log(typeof stringNo);
+
